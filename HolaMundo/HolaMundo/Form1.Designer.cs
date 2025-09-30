5de7790eab64c55070f72358a86fdbdd57e27188
Ian Daniel López Ríos <101910198+id27lr@users.noreply.github.com>
2025-09-30 09:49:03 -0600

    Add files via upload

---
+++ HolaMundo/HolaMundo/Form1.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace HolaMundo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtContrasena1 = new System.Windows.Forms.TextBox();
+            this.txtContrasena2 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(467, 235);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(90, 33);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Validar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(202, 102);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(96, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Contraseña:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(202, 174);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(96, 20);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Contraseña:";
+            // 
+            // txtContrasena1
+            // 
+            this.txtContrasena1.Location = new System.Drawing.Point(331, 102);
+            this.txtContrasena1.Name = "txtContrasena1";
+            this.txtContrasena1.Size = new System.Drawing.Size(226, 26);
+            this.txtContrasena1.TabIndex = 3;
+            // 
+            // txtContrasena2
+            // 
+            this.txtContrasena2.Location = new System.Drawing.Point(331, 171);
+            this.txtContrasena2.Name = "txtContrasena2";
+            this.txtContrasena2.Size = new System.Drawing.Size(226, 26);
+            this.txtContrasena2.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtContrasena2);
+            this.Controls.Add(this.txtContrasena1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtContrasena1;
+        private System.Windows.Forms.TextBox txtContrasena2;
+    }
+}
+
---
+++ HolaMundo/HolaMundo/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HolaMundo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            // Se obtienen las contraseñas de los campos de texto
+            string contrasena = txtContrasena1.Text;
+            string contrasena2 = txtContrasena2.Text;
+
+            // (?=.*[A-Z])      -> al menos una mayúscula
+            // (?=.*[a-z])      -> al menos una minúscula
+            // (?=.*\d)         -> al menos un número
+            // (?=.*[!@#$%^&*]) -> al menos un símbolo de la lista 
+            // .{4,}            -> mínimo 4 caracteres
+
+            string patron = @"^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[!@#$%^&*]).{4,}$";
+
+            bool esValido = Regex.IsMatch(contrasena, patron); // Verifica si la contraseña cumple con el patrón de la expresión regular
+
+            // Mensajes de validación
+            if (!esValido)
+            {
+                MessageBox.Show("La contraseña no cumple con los requisitos.");
+                return;
+            }
+            if(!contrasena.Equals(contrasena2))
+            {
+                MessageBox.Show("Las contraseñas no coinciden.");
+                return;
+            }
+
+            MessageBox.Show("Contraseña válida.");
+
+        }
+    }
+}
